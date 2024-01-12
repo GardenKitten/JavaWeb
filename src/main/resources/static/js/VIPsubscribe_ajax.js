ac8a729d609ab9b23a4b59f1cba57368110e433e
Yu <96760698+GardenKitten@users.noreply.github.com>
2024-01-12 20:59:23 +0800

    upload

--- src/main/resources/static/js/VIPsubscribe_ajax.js
+++ src/main/resources/static/js/VIPsubscribe_ajax.js
@@ -1,5 +1,6 @@
-$(function(){
-    $("input[name='email']").blur(function(){
+$(function () {
+
+    $("input[name='email']").blur(function () {
         // var email = $(this).val();
 
         // if(formObj.checkNull("email","Email不能为空！") && formObj.checkEmail()){
@@ -8,13 +9,13 @@ $(function(){
         // 		$("#email_msg").css("color","red");
         // 	});
         // }
-        formObj.checkNull("email","密码不能为空！");
+        formObj.checkNull("email", "密码不能为空！");
         formObj.checkEmail();
     });
 
-    $("input[name='passw1']").blur(function(){
-        formObj.checkNull("passw1","密码不能为空！");
-       // formObj.checkPassword();
+    $("input[name='passw1']").blur(function () {
+        formObj.checkNull("passw1", "密码不能为空！");
+        // formObj.checkPassword();
 
     });
 
@@ -23,74 +24,92 @@ $(function(){
     //     formObj.checkPassword();
     // });
 
-    $("form").submit(function(){
-        var flag=true;
-        flag=formObj.checkNull("email","Email不能为空！") && flag;
-        flag=formObj.checkNull("passw1","密码不能为空！") && flag;
-       // flag=formObj.checkNull("passw2","确认密码不能为空！") && flag;
-        flag=formObj.checkEmail() && flag;
+    $("form").submit(function () {
+        var flag = true;
+        flag = formObj.checkNull("email", "Email不能为空！") && flag;
+        flag = formObj.checkNull("passw1", "密码不能为空！") && flag;
+        // flag=formObj.checkNull("passw2","确认密码不能为空！") && flag;
+        flag = formObj.checkEmail() && flag;
         //flag=formObj.checkPassword() && flag;
 
         var email = $("input[name='email']").val();
         var passw1 = $("input[name='passw1']").val();
-        if(flag){
-            $.post("/index/doVIPsubscribe",{"email":email,"passw1":passw1},function(result){
-                if(result=="开通成功"){
+        if (flag) {
+            $.post("/index/doVIPsubscribe", {"email": email, "passw1": passw1}, function (result) {
+                if (result == "开通成功") {
                     alert(result);
+                    // this.open1();
                     window.location.href("/index");
-                }else{
+                } else {
+                    // this.open2();
                     alert(result);
                 }
                 $("#form_msg").html(result);
-                $("#form_msg").css("color","red");
+                $("#form_msg").css("color", "red");
             });
         }
         return false;
     });
 });
 
-var formObj={
-    "checkPassword":function(){
-        var passw1=$("input[name='passw1']").val();
-        var passw2=$("input[name='passw2']").val();
-        if(passw1.length>0 &&passw2.length>0){
-            if(passw1!=passw2){
-                this.setMsg("passw1","两次密码不一致！");
-                this.setMsg("passw2","两次密码不一致！");
+var formObj = {
+    "checkPassword": function () {
+        var passw1 = $("input[name='passw1']").val();
+        var passw2 = $("input[name='passw2']").val();
+        if (passw1.length > 0 && passw2.length > 0) {
+            if (passw1 != passw2) {
+                this.setMsg("passw1", "两次密码不一致！");
+                this.setMsg("passw2", "两次密码不一致！");
                 return false;
-            }else{
-                this.setMsg("passw1","");
-                this.setMsg("passw2","");
+            } else {
+                this.setMsg("passw1", "");
+                this.setMsg("passw2", "");
             }
         }
         return true;
     },
-    "checkEmail":function(){
-        var email=$("input[name='email']").val();
-        var regExp=/^\w+@\w+(\.\w+)+$/;
-        if(email.length>0){
-            if(!regExp.test(email)){
-                this.setMsg("email","email格式不正确！");
+    "checkEmail": function () {
+        var email = $("input[name='email']").val();
+        var regExp = /^\w+@\w+(\.\w+)+$/;
+        if (email.length > 0) {
+            if (!regExp.test(email)) {
+                this.setMsg("email", "email格式不正确！");
                 return false;
             }
         }
         return true;
     },
-    "checkNull":function(name,msg){
-        var value =$("input[name='"+ name +"']").val();
-        if(value.length ==0){
-            this.setMsg(name,msg);
+    "checkNull": function (name, msg) {
+        var value = $("input[name='" + name + "']").val();
+        if (value.length == 0) {
+            this.setMsg(name, msg);
             return false;
-        }else{
-            this.setMsg(name,"");
+        } else {
+            this.setMsg(name, "");
         }
         return true;
     },
-    "setMsg":function(name,msg){
-        $("#"+name+"_msg").text(msg);
-        $("#"+name+"_msg").css("color","red");
+    "setMsg": function (name, msg) {
+        $("#" + name + "_msg").text(msg);
+        $("#" + name + "_msg").css("color", "red");
         //		$("#passw2_msg").css("color","red");
     }
 
 }
+
+// export default {
+//     methods: {
+//         open1() {
+//             this.$message('开通成功！');
+//             type: 'success'
+//         },
+//         open2() {
+//             this.$message({
+//                 message: '您已经是会员了！',
+//                 type: 'warning'
+//             });
+//         }
+//     }
+// }
+
 
